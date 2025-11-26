f5ed0ee96216b102d24902cf30b8e4053c5cfc67
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2025-11-26 22:20:05 +0300

    metrics dataclasses

--- src/domain/metrics/metrics.py
+++ src/domain/metrics/metrics.py
@@ -1,12 +1,63 @@
 from dataclasses import dataclass
+from datetime import timedelta
+from enum import Enum
 
+
+class TechFitLevel(str, Enum):
+    """
+    Tech fit level enum
+    """
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+class SeniorityGuess(str, Enum):
+    """
+    Seniority guess enum
+    """
+    JUNIOR = "junior"
+    MIDDLE = "middle"
+    SENIOR = "senior"
+
+class Recommendation(str, Enum):
+    """
+    Recommendation enum
+    """
+    REJECT = "reject"
+    DOUBT = "doubt"
+    HIRE = "hire"
+    STRONG_HIRE = "strong_hire"
 
 @dataclass
-class Metrics:
+class MetricsBlock1:
     """
     Metrics class
     """
+    time_spent: timedelta
+    time_per_task: timedelta
+    answers_count: int
+    copy_paste_suspicion: int
 
-    ...
-    # TODO: Add metrics
+@dataclass
+class MetricsBlock2:
+    """
+    Metrics class
+    """
+    summary: str
+    clarity_score: int # 1-5
+    completeness_score: int # 1-5
+    feedback_response: str
+    tech_fit_level: TechFitLevel
+    tech_fit_comment: str
+@dataclass
+class MetricsBlock3:
+    """
+    Metrics class
+    """
+    strengths: str
+    weaknesses: str
+    cheating_summary: str
+    seniority_guess: SeniorityGuess
+    recommendation: Recommendation
 
+#TODO: Implement tests and code metrics
